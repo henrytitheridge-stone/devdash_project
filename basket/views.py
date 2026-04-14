9cec30e5fd6f3522cea2b8610d46ab8f87ea1bac
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-14 23:56:36 +0100

    Build add_to_basket view, urls and link from service_detail pages

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def add_to_basket(request, item_id):
+
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    basket[item_id] = 1
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
