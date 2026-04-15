99e73e2010aa441625ac9a60060bc09e93f58490
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-15 17:31:38 +0100

    Setup responsive basket template and adjust styling

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('', views.view_basket, name='view_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
