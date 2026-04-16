a48550ce7fc8e6b48ed9f809067445b806e3353c
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-16 05:39:11 +0100

    Implement checkout POST logic to connect checkout_success to payment journey

--- checkout/views.py
+++ checkout/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import Order
+from .models import Order, OrderLineItem
+from services.models import Service
 from basket.contexts import basket_contents
 
 import stripe
@@ -13,10 +14,37 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
 
-    if not basket:
-        return redirect(reverse('services'))
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+        }
+        
+        order_form = OrderForm(form_data)
+
+        if order_form.is_valid():
+            order = order_form.save()
+
+            for item_id in basket.keys():
+                service = Service.objects.get(id=item_id)
+                order_line_item = OrderLineItem(
+                    order=order,
+                    service=service,
+                )
+                order_line_item.save()
+
+            if 'basket' in request.session:
+                del request.session['basket']
+
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            return redirect(reverse('services'))
 
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
