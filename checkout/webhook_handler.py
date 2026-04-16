1949c6e765c57db51f27b615edef0e9b5f4e95ae
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-16 19:29:05 +0100

    Implement robust find-or-create logic in webhook handler, clean up POST logic in views and connect url paths

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from .models import Order, OrderLineItem
+from services.models import Service
+
+import json
+import time
+import stripe
 
 
 class StripeWH_Handler:
@@ -19,8 +25,66 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+
+        intent = event.data.object
+        pid = intent.id
+        basket = intent.metadata.basket
+
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+        billing_details = stripe_charge.billing_details
+        grand_total = round(stripe_charge.amount / 100, 2)
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=billing_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=billing_details.phone,
+                    grand_total=grand_total,
+                    original_basket=basket,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+
+        if order_exists:
+            # self._send_confirmation_email(order)
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=billing_details.name,
+                    email=billing_details.email,
+                    phone_number=billing_details.phone,
+                    original_basket=basket,
+                    stripe_pid=pid,
+                )
+                for item_id in json.loads(basket).keys():
+                    service = Service.objects.get(id=item_id)
+                    OrderLineItem.objects.create(
+                            order=order,
+                            service=service,
+                        )
+                order.update_total()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+            
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
