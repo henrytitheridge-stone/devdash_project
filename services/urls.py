6f1a2a92b1c76379dff7ebe5ee22bea65dd4e5e7
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-17 22:55:12 +0100

    Configure add_service view and urls

--- services/urls.py
+++ services/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_services, name='services'),
     path('<int:service_id>/', views.service_detail, name='service_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Service
+from .forms import ServiceForm
 
 
 # Create your views here.
@@ -24,3 +26,28 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_detail.html', context)
+
+
+@login_required
+def add_product(request):
+
+    if not request.user.is_superuser:
+        # messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            service = form.save()
+
+            return redirect(reverse('service_detail', args=[service.id]))
+
+    else:
+        form = ServiceForm()
+
+    template = 'service/add_service.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
