a1d5de80da090312bb07abc4b333a7d6d8f2ede3
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-17 08:37:26 +0100

    Add UserProfileForm for users to update their phone number

---
+++ profiles/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0'
+            self.fields[field].label = False
