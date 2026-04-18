e7583408f478eb794e5a344001adf4de5776c940
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-18 07:49:40 +0100

    Add success toast with basket preview and include service_count in context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -6,11 +6,13 @@ def basket_contents(request):
 
     basket_items = []
     total = 0
+    service_count = 0
     basket = request.session.get('basket', {})
 
     for item_id in basket.keys():
         service = get_object_or_404(Service, pk=item_id)
         total += service.price
+        service_count += 1
         basket_items.append({
               'item_id': item_id,
               'service': service,
@@ -22,6 +24,7 @@ def basket_contents(request):
         'basket_items': basket_items,
         'total': total,
         'grand_total': grand_total,
+        'service_count': service_count,
     }
 
     return context
