b9c0e72b14c7e582e21720bc332828c5ef90bb3a
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-18 01:59:53 +0100

    Create EnquiryForm for contacting site owner

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Subscriber
+from .models import Subscriber, Enquiry
 
 
 class SubscriberForm(forms.ModelForm):
@@ -12,3 +12,14 @@ class SubscriberForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'Enter your email...'
         self.fields['email'].label = False
         self.fields['email'].widget.attrs['class'] = 'border-black rounded-0'
+
+
+class EnquiryForm(forms.ModelForm):
+    class Meta:
+        model = Enquiry
+        fields = ('name', 'email', 'subject', 'message')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
