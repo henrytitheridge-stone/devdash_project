e70cb569b10096bee45d94c2a0ce35bbe621506b
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-18 01:50:16 +0100

    Initialise and register subscriber and enquiry models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Subscriber, Enquiry
+
 
 # Register your models here.
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_subscribed',)
+    ordering = ('-date_subscribed',)
+
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject', 'date_sent',)
+    ordering = ('-date_sent',)
+
+admin.site.register(Subscriber, SubscriberAdmin)
+admin.site.register(Enquiry, EnquiryAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Subscriber(models.Model):
+    email = models.EmailField(max_length=254, unique=True)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+class Enquiry(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    date_sent = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Enquiries"
+
+    def __str__(self):
+        return f"Enquiry from {self.name}: {self.subject}"
