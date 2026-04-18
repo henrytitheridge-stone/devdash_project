70e52915e09bb11270d0f36cae65a7c44c77fa52
henrytitheridge-stone <henrytitheridgestone@gmail.com>
2026-04-18 02:07:26 +0100

    Add newsletter_signup and contact views

--- home/views.py
+++ home/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import SubscriberForm, EnquiryForm
+from .models import Subscriber, Enquiry
+
 
 # Create your views here.
-
-
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def newsletter_signup(request):
+    """ Handle newsletter subscriptions """
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully subscribed!')
+        else:
+            messages.error(request, 'Could not subscribe. Please check your email.')
+
+    return redirect(request.META.get('HTTP_REFERER', 'home'))
+
+def contact(request):
+    """ View for enquiry form """
+    if request.method == 'POST':
+        form = EnquiryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Enquiry sent! We will be in touch soon.')
+            return redirect(reverse('home'))
+    
+    form = EnquiryForm()
+    return render(request, 'home/contact.html', {'form': form})
